5df07e65fb148bf6d619c3c5cf6b67fc259a726b
Alves Jhonata <jhonatab4@gmail.com>
2023-05-19 16:54:31 -0300

    routes upload img

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import 'dotenv/config'
 import fastify from 'fastify'
+import { resolve } from 'node:path'
 import { authRoutes } from './routes/auth'
 import { memoriesRoutes } from './routes/memories'
+import { uploadRoutes } from './routes/upload'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/upload',
+})
 
 app.register(cors, {
   origin: true, // todas URLs de front-end poderão acessar nosso back-end
@@ -16,11 +26,13 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('🚀 HTTP server runnind on http://localhost:3333')
